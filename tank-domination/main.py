cc3c0164026cf5a4052b52e1ea99e376bf24a67c
[Nate] <[nathanielsal86@gmail.com]>
2022-11-09 00:38:28 -0600

    Tank Domination day 1

---
+++ tank-domination/main.py
@@ -0,0 +1,39 @@
+import pygame
+
+pygame.init()
+
+# Game settings. 
+monitor_display = (800, 600)
+
+game_display = pygame.display.set_mode(monitor_display)
+
+pygame.display.set_caption("Tank Domination")
+
+system_clock = pygame.time.Clock()
+
+game_characteristics = {
+    "sky": {
+        "color": (135, 206, 235)
+    }
+}
+
+# Game logic.
+
+game_running_flag = True
+
+while game_running_flag:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_running_flag = False
+
+    if not game_running_flag:
+        pygame.quit()
+
+        break
+
+    # Running game mechanics.
+    game_display.fill(game_characteristics["sky"]["color"])
+
+    pygame.display.update()
+
+    system_clock.tick(144)
